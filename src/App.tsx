7a092f20de77a1f7174904fe7bc9c6056e2e86eb
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-08 00:43:32 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav/Nav';
+import About from './views/About/About';
+import Contact from './views/Contact/Contact';
+import Home from './views/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <Router>
+    <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
+  </>
 }
 
 export default App;
