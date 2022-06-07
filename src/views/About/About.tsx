0e2924f0a3e7dbb5ef392a461966ee9a51033a84
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-08 00:40:30 +0300

    added views dir - home, contact, about

---
+++ src/views/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return <>
+    <h1>קצת עליי</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, enim nisi perferendis
+       in quas voluptatibus temporibus consequatur, quaerat alias totam itaque iure molestias
+        corporis, reprehenderit tempora earum eius id! Ipsa!</p>
+  </>
+}
+
+export default About
