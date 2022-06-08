8ef608d3225adcc05e989b994d7053ae499baca4
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-08 15:31:27 +0300

    added homepage header

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './home.css'
 
 const Home = () => {
-  return <>
-  <h1>Home</h1>
-  </>
+  return <div className="home-container">
+    <div className="bg-img">
+    <img src="oria.png" alt="oria feuerstein rozen" className="self-img" />
+    </div>
+    <div className="home-content">
+      <h1 className="home-header">מורה פרטית</h1>
+      <p className="content-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Modi corporis, amet architecto beatae ea, aliquid eveniet quod, placeat 
+      sapiente quia dolores veritatis. Libero, quas impedit!</p>
+    </div>
+  </div>
 }
 
 export default Home
