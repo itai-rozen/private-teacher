f1429ce90dffd155fdb7c2e9f9c855634e852e9a
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-13 19:04:36 +0300

    added second page to home component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,14 +3,27 @@ import './home.css'
 
 const Home = () => {
   return <div className="home-container">
-    <div className="bg-img">
-    <img src="oria.png" alt="oria feuerstein rozen" className="self-img" />
+    <div className="first-page page">
+      <div className="bg-img">
+        <img src="oria.png" alt="oria feuerstein rozen" className="self-img" />
+      </div>
+      <div className="home-content">
+        <h1 className="home-header">מורה פרטית</h1>
+        <p className="content-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Modi corporis, amet architecto beatae ea, aliquid eveniet quod, placeat
+          sapiente quia dolores veritatis. Libero, quas impedit!</p>
+      </div>
     </div>
-    <div className="home-content">
-      <h1 className="home-header">מורה פרטית</h1>
-      <p className="content-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-      Modi corporis, amet architecto beatae ea, aliquid eveniet quod, placeat 
-      sapiente quia dolores veritatis. Libero, quas impedit!</p>
+    <div className="second-page page">
+      <div className="home-content">
+        <div className="content-text">
+          <h1 className="home-header">פשוט לדבר מתמטיקה</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ut cupiditate quasi velit minima omnis.</p>
+        </div>
+      </div>
+        <div className="study-img">
+          <img src="study.png" alt="" />
+        </div>
     </div>
   </div>
 }
