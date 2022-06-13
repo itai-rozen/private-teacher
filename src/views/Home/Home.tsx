f37cb98c473f80396e2f6e54b67baf662af04bb0
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-13 22:31:12 +0300

    split home view component to seperate page components. added a showing modal state

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,30 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FirstPage from '../../components/FirstPage/FirstPage'
+import SecondPage from '../../components/SecondPage/SecondPage'
 import './home.css'
-
-const Home = () => {
+const Home : React.FC = () => {
+  const [showContact, setShowContact] = useState(false)
   return <div className="home-container">
-    <div className="first-page page">
-      <div className="bg-img">
-        <img src="oria.png" alt="oria feuerstein rozen" className="self-img" />
-      </div>
-      <div className="home-content">
-        <h1 className="home-header">מורה פרטית</h1>
-        <p className="content-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Modi corporis, amet architecto beatae ea, aliquid eveniet quod, placeat
-          sapiente quia dolores veritatis. Libero, quas impedit!</p>
-      </div>
-    </div>
-    <div className="second-page page">
-      <div className="home-content">
-        <div className="content-text">
-          <h1 className="home-header">פשוט לדבר מתמטיקה</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ut cupiditate quasi velit minima omnis.</p>
-        </div>
-      </div>
-        <div className="study-img">
-          <img src="study.png" alt="" />
-        </div>
-    </div>
+    <FirstPage />
+    <SecondPage showContact={showContact} setShowContact={setShowContact} />
   </div>
 }
 
