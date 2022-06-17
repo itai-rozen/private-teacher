e6c8d85a40fb9b01ae7b0a014df61b7371f8490f
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-17 17:44:15 +0300

    home component now wrapped by react fragment instead of div

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,10 +4,10 @@ import SecondPage from '../../components/SecondPage/SecondPage'
 import './home.css'
 const Home : React.FC = () => {
   const [showContact, setShowContact] = useState(false)
-  return <div className="home-container">
+  return <>
     <FirstPage />
     <SecondPage showContact={showContact} setShowContact={setShowContact} />
-  </div>
+  </>
 }
 
 export default Home
