ebb45891f61e9f7009223a594411f5383fbe698e
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-19 22:07:40 +0300

    added third page omponent to homepage

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import FirstPage from '../../components/FirstPage/FirstPage'
 import SecondPage from '../../components/SecondPage/SecondPage'
+import ThirdPage from '../../components/ThirdPage/ThirdPage'
 import './home.css'
 const Home : React.FC = () => {
   const [showContact, setShowContact] = useState(false)
   return <>
     <FirstPage />
     <SecondPage showContact={showContact} setShowContact={setShowContact} />
+    <ThirdPage showContact={showContact} setShowContact={setShowContact}  />
   </>
 }
 
