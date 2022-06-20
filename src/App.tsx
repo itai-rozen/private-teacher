81d3ee435c3aaa142d8d6ec4b011c59fad8aba6f
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-20 14:11:50 +0300

    clean main app from firebase configs & funcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+import { useState  } from 'react';
 import Nav from './components/Nav/Nav';
 import About from './views/About/About';
 import Contact from './views/Contact/Contact';
 import Home from './views/Home/Home';
+import { loginUser } from './firestore'
 
 function App() {
-  return <>
+  console.log(process.env.REACT_APP_ADMIN_EMAIL)
+return <>
     <Router>
     <Nav />
       <Routes>
