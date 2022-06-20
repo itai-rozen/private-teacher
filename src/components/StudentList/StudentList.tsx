6ba3ce6eae3dc7cbdd88cc1bd196a83dcb6a06e3
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-20 14:14:08 +0300

    added student list component for admin

---
+++ src/components/StudentList/StudentList.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import { getStudents, deleteStudent } from '../../firestore'
+import './studentList.css'
+
+const StudentList:React.FC = () => {
+  return <div className="students-container">
+    
+  </div>
+}
+
+export default StudentList
