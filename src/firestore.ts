a07544a68014f18f4c1fc9dd481b54ea2544ef00
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-20 12:05:28 +0300

    added firebase db with studnts collection, added options for getting, adding & deleting students

---
+++ src/firestore.ts
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+const firebaseConfig = {
+  apiKey: "AIzaSyB9YezeGANZ-kIgoPIMHImddQJbqXmrn8g",
+  authDomain: "private-teacher-f6eaa.firebaseapp.com",
+  projectId: "private-teacher-f6eaa",
+  storageBucket: "private-teacher-f6eaa.appspot.com",
+  messagingSenderId: "806552303029",
+  appId: "1:806552303029:web:d944a997537ec3c695d5fc"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore()
+const collectionRef = collection(db, 'students')
+const getStudents = async (): Promise<any> => {
+  try {
+    const studentDocs = await getDocs(collectionRef)
+    const studentObjs = studentDocs.docs.map(doc => {
+      return {
+        ...doc.data(),
+        id: doc.id
+      }
+    })
+    return studentObjs
+  } catch (err) {
+    return { error: err }
+  }
+}
+const addStudent = async (studentObj: { fullName: String, phone: String, notes: String }): Promise<any> => {
+  try {
+    await addDoc(collectionRef, studentObj)
+  } catch (err) {
+    return { error: err }
+  }
+}
+
+const deleteStudent = async (id:string): Promise<any> => {
+  try {
+    await deleteDoc(doc(db, 'students', id))
+  }catch(err){
+    return {error: err}
+  }
+}
+// 
+
+export { addStudent, getStudents, deleteStudent }
+
